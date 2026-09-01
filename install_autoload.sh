d3f6129b9c063ab47ccdc73fdb28ace34a358574
Wjatscheslaw Michailov <mikhailowvw@gmail.com>
2026-09-01 21:32:13 +0300

    Add default config

--- install_autoload.sh
+++ install_autoload.sh
@@ -9,6 +9,8 @@ IMAGE=${1}
 ORDINAL=${2:-1}
 SERVICE_NAME=${IMAGE}-${ORDINAL}
 SUBUID_BLOCK_INDEX=${3:-0}
+BASE_DIR=${4:-"${HOME}"}
+LOG_LEVEL=${5:-warn}
 TEMPLATE_FILE=mtproxy_run.sh
 
 if [ -z "${ORDINAL}" ]; then
@@ -31,7 +33,7 @@ Description=Safe & Secure $IMAGE Service ($SERVICE_NAME)
 After=network.target
 
 [Service]
-ExecStart=$HOME/.local/bin/${SERVICE_NAME}_run.sh $SERVICE_NAME $IMAGE $SUBUID_BLOCK_INDEX
+ExecStart=$HOME/.local/bin/${SERVICE_NAME}_run.sh $SERVICE_NAME $IMAGE $SUBUID_BLOCK_INDEX $LOG_LEVEL $BASE_DIR
 #ExecStartPost=
 ExecStop=/usr/bin/podman stop --ignore $SERVICE_NAME
 ExecStopPost=/usr/bin/podman rm --force --ignore $SERVICE_NAME
